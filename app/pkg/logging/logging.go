2cc93dfe2aa7ca55976c4de76e3377a73204d8ae
Вадим <vadim@MBP-Vadim.NEOTECH>
2025-06-12 15:10:48 +0500

    logger formatting

--- app/pkg/logging/logging.go
+++ app/pkg/logging/logging.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"path"
 	"runtime"
+	"strings"
 	"time"
 
 	"github.com/sirupsen/logrus"
@@ -48,27 +49,65 @@ func GetLogger(ctx context.Context) Logger {
 	return loggerFromContext(ctx)
 }
 
+// CallerHook это хук для исправления caller'а в логах
+type CallerHook struct{}
+
+func (hook *CallerHook) Levels() []logrus.Level {
+	return logrus.AllLevels
+}
+
+func (hook *CallerHook) Fire(entry *logrus.Entry) error {
+	// Получаем стек вызовов
+	pc := make([]uintptr, 15)
+	n := runtime.Callers(8, pc) // Начинаем с 8-го фрейма, чтобы пропустить логгер
+	frames := runtime.CallersFrames(pc[:n])
+	
+	// Ищем первый фрейм, который не относится к логгеру или logrus
+	for {
+		frame, more := frames.Next()
+		if !more {
+			break
+		}
+		
+		// Пропускаем фреймы из пакета logging и logrus
+		if !strings.Contains(frame.File, "/pkg/logging/") && 
+		   !strings.Contains(frame.File, "/logrus/") &&
+		   !strings.Contains(frame.File, "github.com/sirupsen/logrus") {
+			entry.Caller = &runtime.Frame{
+				File: frame.File,
+				Line: frame.Line,
+				Function: frame.Function,
+				PC: frame.PC,
+			}
+			break
+		}
+	}
+	
+	return nil
+}
+
 func NewLogger() Logger {
 	logrusLogger := logrus.New()
 	logrusLogger.SetLevel(logrus.InfoLevel)
 	logrusLogger.SetReportCaller(true)
+	
+	// Добавляем хук для исправления caller'а
+	logrusLogger.AddHook(&CallerHook{})
+	
 	logrusLogger.Formatter = &logrus.TextFormatter{
 		CallerPrettyfier: func(f *runtime.Frame) (string, string) {
 			filename := path.Base(f.File)
-			return fmt.Sprintf("%s:%d", filename, f.Line), fmt.Sprintf("%s()", f.Function)
+			return fmt.Sprintf("%s:%d", filename, f.Line), ""
 		},
 		DisableColors:   false,
 		FullTimestamp:   true,
 	}
 
 	logrusLogger.SetOutput(os.Stdout)
-	logrusLogger.SetReportCaller(true)
 
-	 return&logger{
+	return &logger{
 		Logger: logrusLogger,
 	}
-
-	
 }
 
 // SetLevel sets the logging level
